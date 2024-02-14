6fa66fa1d56269ae13c9952eddcbf595f0cf6c6f
Vitor Porto <vhpporto@gmail.com>
2024-02-13 21:40:33 -0300

    feat: add `constants`

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const PERSONAL_LOAN = {
+  type: "PERSONAL",
+  interest_rate: 4,
+};
+export const GUARANTEED_LOAN = {
+  type: "GUARANTEED",
+  interest_rate: 4,
+};
+export const CONSIGNMENT_LOAN = {
+  type: "CONSIGNMENT",
+  interest_rate: 4,
+};
